98d85ed1b56bbe1d8cc0667c7258f00068a7ddcd
RakibCSE <rakibul.hossain365@gmail.com>
2019-12-06 02:37:43 +0600

    models serializers

---
+++ productapi/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+
+from .models import Product, Attribute, Price
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    """
+    Product serializer
+    """
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class AttributeSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Attribute serializer to show the product attributes list.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        For multiple attributes used many=True
+        Args:
+            *args:
+            **kwargs:
+        """
+        many = kwargs.pop('many', True)
+        super(AttributeSerializer, self).__init__(many=many, *args, **kwargs)
+
+    class Meta:
+        model = Attribute
+        fields = ('color', 'size', 'product',)
+
+
+class PriceSerializer(serializers.HyperlinkedModelSerializer):
+    """
+    Price serializer to show the prices over the API call.
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        For multiple attributes used many=True
+        Args:
+            *args:
+            **kwargs:
+        """
+        many = kwargs.pop('many', True)
+        super(PriceSerializer, self).__init__(many=many, *args, **kwargs)
+
+    class Meta:
+        model = Price
+        fields = ('product', 'price', 'start_date', 'end_date',)
