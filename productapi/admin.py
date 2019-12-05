722c4c6faa0e78cd10164370b9e8bcb2a5e68c80
RakibCSE <rakibul.hossain365@gmail.com>
2019-12-06 02:36:40 +0600

    model registered

--- productapi/admin.py
+++ productapi/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Attribute, Price
+
+
+admin.site.register(Product)
+admin.site.register(Attribute)
+admin.site.register(Price)
