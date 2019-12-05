75ed7b57d276c6a3c8796774e410b4fa08eb687b
RakibCSE <rakibul.hossain365@gmail.com>
2019-12-06 02:38:28 +0600

    api list and detail views

--- productapi/views.py
+++ productapi/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product, Attribute, Price
+from .serializers import ProductSerializer, AttributeSerializer, PriceSerializer
+
+
+class ProductList(generics.ListCreateAPIView):
+    """
+    Product list API View.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Product detail API View.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class AttributeList(generics.ListCreateAPIView):
+    """
+    Attribute list API View.
+    """
+    queryset = Attribute.objects.all()
+    serializer_class = AttributeSerializer
+
+
+class AttributeDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Attribute detail API View.
+    """
+    queryset = Attribute.objects.all()
+    serializer_class = AttributeSerializer
+
+
+class PriceList(generics.ListCreateAPIView):
+    """
+    Price list API View.
+    """
+    queryset = Price.objects.all()
+    serializer_class = PriceSerializer
+
+
+class PriceDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Price detail API View.
+    """
+    queryset = Price.objects.all()
+    serializer_class = PriceSerializer
