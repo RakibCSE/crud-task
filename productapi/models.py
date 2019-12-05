a302116a59c6e349f0bc36ee9326d7845608d240
RakibCSE <rakibul.hossain365@gmail.com>
2019-12-06 02:37:22 +0600

    created product, attribute and price model

--- productapi/models.py
+++ productapi/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=250)
+    code = models.CharField(max_length=10)
+    slug = models.SlugField(max_length=250)
+    available = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+
+
+class Attribute(models.Model):
+    product = models.ForeignKey(Product, related_name="attributes", on_delete=models.PROTECT)
+    color = models.CharField(max_length=15)
+    size = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.color + " " + self.size
+
+
+class Price(models.Model):
+    product = models.ForeignKey(Product, related_name="prices", on_delete=models.PROTECT)
+    price = models.IntegerField()
+    start_date = models.DateField(null=True)
+    end_date = models.DateField(null=True)
+
+    def __str__(self):
+        return str(self.price)
