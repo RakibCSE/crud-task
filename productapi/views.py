522226218dd7452c45d922b183e18d312c169128
RakibCSE <rakibul.hossain365@gmail.com>
2019-12-06 16:46:20 +0600

    rest framework Response import removed

--- productapi/views.py
+++ productapi/views.py
@@ -1,5 +1,4 @@
 from rest_framework import generics
-from rest_framework.response import Response
 
 from .models import Product, Attribute, Price
 from .serializers import ProductSerializer, AttributeSerializer, PriceSerializer
